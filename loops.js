c8a4e7865a0e9d6b9d0cdbffd427e7edc1090ec8
SnigdhaReddy09 <93863995+SnigdhaReddy09@users.noreply.github.com>
2024-10-24 11:02:47 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,94 @@
+// for loop
+// increment
+for(var a=1;a<3;a++){
+    console.log(a)
+}
+// 1,2
+
+for(var a=1;a<=7;a++){
+    console.log(a)
+}
+// 1-7
+
+for(var a=1;a<=10;a++){
+    console.log(a)
+}
+// 1-10
+
+for(var a=7;a<12;a++){
+    console.log(a)
+}
+// 7-11
+
+for(var a=17;a<3;a++){
+    console.log(a)
+}
+// no output
+
+// decrement
+for(var a=11;a>6;a--){
+    console.log(a)
+}
+// 11-7
+
+for(var a=24;a>=15;a--){
+    console.log(a)
+}
+// 24-15
+
+// ***
+for(var i=0;i<=5;i++){
+
+}
+console.log(i)
+// 6
+
+// while loop
+// increment
+var a=1;
+while(a<=7){
+    console.log(a)
+    a++
+}
+// 1-7
+
+// decrement
+var a=10;
+while(a>=6){
+    console.log(a)
+    a--
+}
+// 10-6
+
+// ***
+var i=1;
+while(i<4){
+    i++
+}
+console.log(i)
+// 4
+
+// do-while
+// increment
+var i=1;
+do{
+i++
+console.log(i)
+}while(i<=5);
+// 2-6
+
+// decrement
+var a=10;
+do{
+a--
+console.log(a)
+}while(a>=7);
+// 9-6
+
+// ***
+var i=10;
+do{
+console.log(i)
+i--
+}while(i>=7);
+// 10-7
